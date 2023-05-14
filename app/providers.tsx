cb3892cc3c1e7e21c856318adbb568d7802bae82
Anton Pomogaev <antonpomogaev@gmail.com>
2023-05-14 21:33:01 +0300

    feat: add navbar

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import type { ReactNode } from 'react';
+
+type Props = {
+  children?: ReactNode;
+};
+
+const Providers = ({ children }: Props) => {
+  return <>{children}</>;
+};
+export default Providers;
